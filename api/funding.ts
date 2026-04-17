a1afe19d86fc9d6402d8067280eafaa24e9a4106
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-17 17:29:00 +0530

    Update funding.ts

--- api/funding.ts
+++ api/funding.ts
@@ -5,7 +5,13 @@ export default function handler(req, res) {
   try {     
      
 
-    return "This is just a test";
+    res.status(200).json({
+            status: "Active",
+            balance: 5000,
+            dueDate: "2026-05-15",
+            type: "Personal"
+        });
+    
   } catch (error) {
     console.error("Error fetching/parsing CSV:", error);
     throw error;
