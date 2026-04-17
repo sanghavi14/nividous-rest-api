928bab1dd6dbe35a8d2918f294b750addf49e107
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-17 17:35:06 +0530

    Update funding.ts

--- api/funding.ts
+++ api/funding.ts
@@ -1,16 +1,25 @@
-//import axios from "axios";
-//import csv from "csvtojson";
+import * as fs from 'fs';
+import * as path from 'path';
+import { parse } from 'csv-parse';
 
 export default function handler(req, res) {
   try {     
      
 
-    res.status(200).json({
+ /*   res.status(200).json({
             status: "Active",
             balance: 5000,
             dueDate: "2026-05-15",
             type: "Personal"
-        });
+        }); */
+  
+    
+    const csvFilePath = path.resolve(__dirname, 'data.csv');
+    const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+    parse(fileContent, { delimiter: ',' }, (err, records) => {
+      res.status(200).json({records});
+    });
+      
     
   } catch (error) {
     console.error("Error fetching/parsing CSV:", error);
