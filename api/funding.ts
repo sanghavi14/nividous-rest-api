ad78d656069c5f55c81cca2a503202eb55091cc2
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-17 17:27:22 +0530

    Update funding.ts

--- api/funding.ts
+++ api/funding.ts
@@ -1,17 +1,11 @@
-import axios from "axios";
-import csv from "csvtojson";
+//import axios from "axios";
+//import csv from "csvtojson";
 
 export default function handler(req, res) {
   try {     
-    const csvUrl = "LoanInformationData.csv";
+     
 
-    // Fetch CSV file
-    const response = await axios.get(csvUrl);
-
-    // Convert CSV to JSON
-    const jsonArray = await csv().fromString(response.data);
-
-    return jsonArray;
+    return "This is just a test";
   } catch (error) {
     console.error("Error fetching/parsing CSV:", error);
     throw error;
