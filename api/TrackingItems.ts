2c79d044802048e1b9bb9839541c1ec6a13983ca
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-20 14:57:20 +0530

    Update TrackingItems.ts

--- api/TrackingItems.ts
+++ api/TrackingItems.ts
@@ -62,13 +62,14 @@ export default function handler(req, res) {
               break;          
             }
           }  
-        }      
-      }*/
-      const records = tiRecords.filter(r => r.loan_number === loanNumber);  
-      if (records.length > 0) {
-        res.status(200).json(records);
-      } else {
-        res.status(404).json({ message: "No records found for this loan number" });
-      } 
+        } */    
+      }
+    
+      //const records = tiRecords.filter(r => r.loan_number === loanNumber);  
+      //if (records.length > 0) {
+       // res.status(200).json(records);
+      //} else {
+        //res.status(404).json({ message: "No records found for this loan number" });
+      //} 
    // }
 }
