451ea1c5958d071fe199b2c8a1a32c5e6efc06e6
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-20 22:59:28 +0530

    Update WireBankContacts.ts

--- api/WireBankContacts.ts
+++ api/WireBankContacts.ts
@@ -1,10 +1,10 @@
 import data,{setWbcData, getWireBankContacts} from "./data.js";
 
 export default function handler(req, res) {       
-   const { loanNumber,operation,wbContact} = req.query;
-     
+   const { loanNumber,operation} = req.query;
+   const wbContactBody = req.body;   
    if(operation == "set"){
-        setWbcData(loanNumber, wbContact);        
+        setWbcData(loanNumber, wbContactBody);        
    }   
    const records = getWireBankContacts(loanNumber);
    if (records.length > 0) {
