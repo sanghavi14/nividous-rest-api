3289b1508def1a493806cd415b3d82f95738d69d
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-21 12:31:26 +0530

    Update TrackingItems.ts

--- api/TrackingItems.ts
+++ api/TrackingItems.ts
@@ -36,10 +36,15 @@ export default function handler(req, res) {
     { id: "t29", loan_number: "0100578919", ti_name: "Payoff Shortage", ti_status: "In Process", prior_to: "Funded-Funds Disbured", due_date: "17-02-2026", has_exception: "yes" }
   ];
   
-  const { loanNumber,operation,tiNames } = req.query;  
+  const { loanNumber,operation,tiNames, } = req.query;  
   // Get all records for this loan
   if(operation == "set"){
         setTiData(loanNumber, tiNames);        
+  }
+
+  if(operation == "update"){   
+       const { loanNumber,operation,tiName, tiStatus,dueDate,hasExcp,priorTo} = req.query; 
+       setTrackingItemData(loanNumber,tiName,tiStatus,dueDate,hasExcp,priorTo);        
   }
   //const records = tiRecords.filter(r => r.loan_number === loanNumber);  
    const records = getTrackingItemsData(loanNumber);
