31ba0e429cd52638fdbfad2f54a81aa2fd934685
imrajkirpalsinh <63632366+imrajkirpalsinh@users.noreply.github.com>
2026-04-22 16:50:48 +0530

    Update data.js

--- api/data.js
+++ api/data.js
@@ -4,7 +4,7 @@ var data = {
 };
 
 var tiRecords = [
-    { id: "t1", loan_number: "0100578962", ti_name: "Funding Rework", ti_status: "Requested", prior_to: "Funded-Funds Disbursed", due_date: "01-01-2026", has_exception: "No" },
+    { id: "t1", loan_number: "0100578962", ti_name: "Funding Rework", ti_status: "In Process", prior_to: "Funded-Funds Disbursed", due_date: "01-01-2026", has_exception: "No" },
     { id: "t2", loan_number: "0100578972", ti_name: "Funding Rework", ti_status: "Requested", prior_to: "Funded-Funds Disbured", due_date: "17-01-2026", has_exception: "yes" },
     { id: "t3", loan_number: "0100578982", ti_name: "Funding Rework", ti_status: "Requested", prior_to: "Funded-Funds Disbured", due_date: "07-01-2026", has_exception: "no" },
     { id: "t4", loan_number: "0100578992", ti_name: "Funding Rework", ti_status: "Initiated", prior_to: "Funded-Funds Disbured", due_date: "05-01-2026", has_exception: "yes" },
@@ -15,7 +15,7 @@ var tiRecords = [
     { id: "t23", loan_number: "0100578914", ti_name: "Funding Rework", ti_status: "Initiated", prior_to: "Funded-Funds Disbured", due_date: "11-01-2026", has_exception: "yes" },
     { id: "t24", loan_number: "0100578919", ti_name: "Funding Rework", ti_status: "Initiated", prior_to: "Funded-Funds Disbured", due_date: "11-01-2026", has_exception: "yes" },
   
-    { id: "t7", loan_number: "0100578962", ti_name: "CD Post Closing Adjustments", ti_status: "Requested", prior_to: "Post Closing Purchase Complete", due_date: "22-03-2026", has_exception: "no" },
+    { id: "t7", loan_number: "0100578962", ti_name: "CD Post Closing Adjustments", ti_status: "In Process", prior_to: "Post Closing Purchase Complete", due_date: "22-03-2026", has_exception: "no" },
     { id: "t8", loan_number: "0100578972", ti_name: "CD Post Closing Adjustments", ti_status: "In Process", prior_to: "Post Closing Purchase Complete", due_date: "23-04-2026", has_exception: "yes" },
     { id: "t9", loan_number: "0100578982", ti_name: "CD Post Closing Adjustments", ti_status: "In Process", prior_to: "Post Closing Purchase Complete", due_date: "03-02-2026", has_exception: "no" },
     { id: "t10", loan_number: "0100578992", ti_name: "CD Post Closing Adjustments", ti_status: "Requested", prior_to: "Post Closing Purchase Complete", due_date: "10-03-2026", has_exception: "yes" },
